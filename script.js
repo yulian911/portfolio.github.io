08a73d79cd72220f6963045117519c8067329fe5
yulian911 <74702716+yulian911@users.noreply.github.com>
2021-03-23 21:27:30 +0100

    dodanie section Gallery

--- script.js
+++ script.js
@@ -76,6 +76,31 @@ $(document).ready(function(){
         }
     });
 });
+// slider 
+var slides = document.querySelectorAll('.slide');
+var btns =document.querySelectorAll('.btn')
+let currentSlide=1;
+
+// manualna nawigajcja
+var manualNav=function(manual){
+
+    slides.forEach((slide)=>{
+    slide.classList.remove('active');
+    btns.forEach((btn)=>{
+    btn.classList.remove('active');
+       });
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active')
+}
+btns.forEach((btn,i)=>{
+    btn.addEventListener("click",()=>{
+        manualNav(i);
+        currentSlide=i;
+
+    });
+})
 // //animacja ruchu
 // const card = document.querySelector('.card');
 // const card1 = document.querySelector('.item1');
